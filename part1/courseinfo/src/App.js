72b0e4634c8a82453af83e75cc5885a1ff79cd96
EthanDarling8 <edarling90@gmail.com>
2021-07-29 15:35:25 -0600

    Event, State Handling (30 Min)

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,5 +1,20 @@
-import React from "react";
+import React, {useState} from "react";
 
+const App = () => {
+    const [counter, setCounter] = useState(0)
+    const increaseByOne = () => setCounter(counter + 1)
+    const setToZero = () => setCounter(0)
+
+    return (
+        <div>
+            <div>{counter}</div>
+            <button onClick={increaseByOne}>plus</button>
+            <button onClick={setToZero}>zero</button>
+        </div>
+    )
+}
+
+/*
 const App = () => {
     const course = {
         name: "Half Stack application development",
@@ -22,11 +37,28 @@ const App = () => {
     return (
         <div>
             <Header course={course.name}/>
+            <Hello name={"Ethan"} age={30}/>
             <Content parts={course.parts}/>
             <Total parts={course.parts}/>
         </div>
 
     );
+}
+
+const Hello = ({name, age}) => {
+
+    const bornYear = () => new Date().getFullYear() - age
+
+    return (
+        <div>
+            <p>
+                Hello {name}, you are {age} years old.
+            </p>
+            <p>
+                So you were probably born in {bornYear()}
+            </p>
+        </div>
+    )
 }
 
 const Header = (props) => {
@@ -59,6 +91,7 @@ const Part = (props) => {
         <p>{props.name} {props.exercises}</p>
     )
 }
+*/
 
 
 export default App;
